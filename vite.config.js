e484d4d668b6f1a7be92185bfb583a2433d7ed87
E. Andrew Miracle <eandrewmiracle@gmail.com>
2025-10-23 01:27:26 +0100

    Fix GitHub Pages 404 redirect and base path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Nailand.docs/", 
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    historyApiFallback: true,
-  },
 });
